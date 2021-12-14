c0e68f6b20a4c6dff02ca9180dac6f9f5da8ad7c
Jacob Larson <jacob.larson55112@gmail.com>
2021-12-13 22:17:53 -0600

    ruined project, ramoizer for people needs fixed

--- client.js
+++ client.js
@@ -1,25 +1,45 @@
 let answer;
+
 console.log('Here are all the available people:', people);
 $(ready)
 function ready(){
     randomCharacter();
     render();
-    $(document).on('click', 'img', playerAttempt)
+    $(document).on('click', 'img', playerAttempt);
+    $('body').css('background-image', `url("<img data-name="${answer.name}" 
+        src="https://github.com/${answer.githubUsername}.png?" 
+        alt="Profile image of ${answer.name}></img>")`)
 }
+
 
 
 function render(){
-    for(let person of people){
+    let copycat = people
+    //loop through copy arry
+    for(let index in copycat){
+        //use random index between 0 and copycat length and append to dom
+        index = Math.floor(Math.random() * (copycat.length - 1));
+        console.log('index,', index);
+        console.log(index, index, index,);
         $('#folks').append(`
-        <div>
-            <img data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
-        </div>
-    `);
+            <div>
+                <img data-name="${copycat[index].name}" 
+                src="https://github.com/${copycat[index].githubUsername}.png?size=250" 
+                alt="Profile image of ${copycat[index].name}">
+            </div>
+        `);
+        //delete index so no repeats
+        copycat.splice(index, 1) /// giving me random index?????.... was using wrong arr.. going to sleep now.
+        console.log(copycat.length, i);
+        console.log(copycat);
     }
 }
 
+
+
 function randomCharacter(remove){
     let character = people[Math.floor(Math.random() * people.length) + 1];
+
     answer = character;
     window.alert(`Please find ${character.name}`)
     console.log(character);
@@ -27,6 +47,8 @@ function randomCharacter(remove){
         src="https://github.com/${answer.githubUsername}.png?size=250" 
         alt="Profile image of ${answer.name}></img>")`)
 }
+
+
 
 function playerAttempt(){
     if($(this).data('name') === answer.name){
@@ -39,6 +61,8 @@ function playerAttempt(){
     }
     console.log($(this).data('name'));
 }
+
+
 
 function playAgain(){
     let anotherRound = window.confirm('Play again?')
